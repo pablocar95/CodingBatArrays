0220e0c372414f741ce98c2b2dc05bb5fa7f2705
pablo <pablo@pablo-TOSH>
2015-01-26 10:06:25 +0100

    Ejercicio MiddleWay por pablo

--- src/VentanaCodingBat.java
+++ src/VentanaCodingBat.java
@@ -64,7 +64,9 @@ public class VentanaCodingBat extends javax.swing.JFrame {
                return c;
      
     }//GEN-LAST:event_jButton1MousePressed
-   
+   //
+   //acabado
+   //
     public static void main(String args[]) {
         /* Set the Nimbus look and feel */
         //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
